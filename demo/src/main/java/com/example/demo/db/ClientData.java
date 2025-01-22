2212bcc332a4f845e6da37e7848935cd17d2a219
Renos Bardis <renosbardis@gmail.com>
2025-01-22 20:05:55 +0200

    feature: Adding validations

--- demo/src/main/java/com/example/demo/db/ClientData.java
+++ demo/src/main/java/com/example/demo/db/ClientData.java
@@ -15,11 +15,11 @@ public class ClientData {
     @Bean
     public CommandLineRunner initDB(ClientRepository clientRepository) {
         return args -> {
-            // Create a list of clients to be saved
+            // Create a list of clients without specifying IDs
             List<Client> clients = Arrays.asList(
-                    new Client(1L, "Renos", "Bardis", "123 Main St", "renos@example.com", "Greek", 35),
-                    new Client(2L, "John", "Doe", "456 Elm St", "john.doe@example.com", "American", 40),
-                    new Client(3L, "Nick", "Smith", "789 Oak St", "nick.smith@example.com", "British", 28)
+                    new Client(null, "Renos", "Bardis", "123 Main St", "renos@example.com", "Greece", 35),
+                    new Client(null, "John", "Doe", "456 Elm St", "john.doe@example.com", "Greece", 40),
+                    new Client(null, "Nick", "Smith", "789 Oak St", "nick.smith@example.com", "Greece", 28)
             );
 
             // Save all clients to the database
